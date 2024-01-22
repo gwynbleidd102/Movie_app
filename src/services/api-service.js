65a86b9805966bf29678e94d9eb7cf2321105c18
Азамат Хасанов <hsm99@mail.ru>
2024-01-22 21:25:28 +0500

    Правки 2

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -2,52 +2,53 @@ export default class ApiService {
   url = 'https://api.themoviedb.org'
   apiKey = 'bd60764de107367b21c4e6a1439fc831'
 
+  options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZDYwNzY0ZGUxMDczNjdiMjFjNGU2YTE0MzlmYzgzMSIsInN1YiI6IjY1ODgwMDgwZTI5NWI0NzA1MzU4NjUzNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6CQMtQm4ilwQpbOoH4vGB-BIBKy3sS5um0WfHsdi498',
+    },
+  }
+
   getResource = async (url, page, searchText, payload, body) => {
-    try {
-      let fetchUrl = new URL(url, this.url)
-      fetchUrl.searchParams.append('api_key', this.apiKey)
-
-      if (page) {
-        fetchUrl.searchParams.append('page', page)
-      }
-
-      fetchUrl.searchParams.append('language', 'en-US')
-
-      if (searchText) {
-        searchText = searchText.trim()
-        fetchUrl.searchParams.append('query', searchText)
-      }
-
-      if (payload) {
-        for (let key in payload) {
-          fetchUrl.searchParams.append(key, payload[key])
-        }
-      }
-
-      let res
-
-      if (body) {
-        res = await fetch(fetchUrl, {
-          headers: {
-            'Content-Type': 'application/json;charset=utf-8',
-            accept: 'application/json',
-          },
-          method: 'POST',
-          body: JSON.stringify({ ...body }),
-        })
-      } else {
-        res = await fetch(fetchUrl)
-      }
-
-      if (!res.ok) {
-        throw new Error(`Request failed with status ${res.status}`)
-      }
-
-      return await res.json()
-    } catch (error) {
-      console.error('API Error:', error.message)
-      throw error
+    let fetchUrl = new URL(url, this.url)
+    fetchUrl.searchParams.append('api_key', this.apiKey)
+    if (page) {
+      fetchUrl.searchParams.append('page', page)
     }
+
+    fetchUrl.searchParams.append('language', 'en-US')
+
+    if (searchText) {
+      searchText = searchText.trim()
+      fetchUrl.searchParams.append('query', searchText)
+    }
+
+    if (payload) {
+      for (let key in payload) {
+        fetchUrl.searchParams.append(key, payload[key])
+      }
+    }
+    let res
+    if (body) {
+      res = await fetch(fetchUrl, {
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+          accept: 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify({ ...body }),
+      })
+    } else {
+      res = await fetch(fetchUrl)
+    }
+
+    if (!res.ok) {
+      throw new Error(`Ошибка HTTP: ${res.status}`)
+    }
+
+    return await res.json()
   }
 
   createRatedList = (movieList, ratedList) => {
@@ -56,16 +57,19 @@ export default class ApiService {
     if (ratedList) {
       let ratedMovies = ratedList.results
       let resultList = movies.map((movie) => {
-        let isRated = ratedMovies.some((ratedMovie) => ratedMovie.id === movie.id)
-
+        let isRated = ratedMovies.some((ratedMovie) => {
+          return ratedMovie.id === movie.id
+        })
         if (isRated) {
-          let ratedMovie = ratedMovies.find((ratedMovie) => ratedMovie.id === movie.id)
+          let ratedMovie = ratedMovies.find((ratedMovie) => {
+            return ratedMovie.id === movie.id
+          })
+
           movie.rating = ratedMovie.rating
         }
 
         return movie
       })
-
       movieList.results = resultList
     }
 
@@ -78,44 +82,85 @@ export default class ApiService {
     return this.getResource(`${this.url}/3/authentication/guest_session/new`)
   }
 
+  // createGuestSession = async () => {
+  //   try {
+  //     const response = await this.getResource('/3/authentication/guest_session/new')
+  //     if (!response.success) {
+  //       throw new Error('Ошибка при создании гостевой сессии')
+  //     }
+  //     return response
+  //   } catch (error) {
+  //     console.error('Ошибка при создании гостевой сессии:', error)
+  //     throw error
+  //   }
+  // }
+
   getMovies = async (text, page, guestKey) => {
     let urlParameter
-
     if (text) {
       urlParameter = '/3/search/movie'
     } else {
       urlParameter = '/3/movie/popular'
     }
+    try {
+      const movies = await this.getResource(urlParameter, page, text)
+      let ratedMovies
+      if (guestKey) {
+        ratedMovies = await this.getRatedMovies(1, guestKey)
+      }
 
-    const movies = await this.getResource(urlParameter, page, text)
-    let ratedMovies
-
-    if (guestKey) {
-      ratedMovies = await this.getRatedMovies(1, guestKey)
+      return this.createRatedList(movies, ratedMovies)
+    } catch (error) {
+      console.error('Ошибка при получении фильмов:', error)
+      throw error
     }
-
-    return this.createRatedList(movies, ratedMovies)
   }
 
-  setRating = (movieId, rating, guestKey) => {
+  setRating = async (movieId, rating, guestKey) => {
     const ratingUrl = `/3/movie/${movieId}/rating`
-
     const guestSessionKey = {
       guest_session_id: guestKey,
     }
-
     const body = { value: rating }
 
-    return this.getResource(ratingUrl, null, null, guestSessionKey, body)
+    try {
+      const response = await this.getResource(ratingUrl, null, null, guestSessionKey, body)
+      if (!response.success) {
+        throw new Error('Оценка фильма не удалась')
+      }
+      return response
+    } catch (error) {
+      console.error('Ошибка при оценке фильма:', error)
+      throw error
+    }
   }
 
-  getRatedMovies = (page, guestKey) => {
+  getRatedMovies = async (page, guestKey) => {
     const url = `/3/guest_session/${guestKey}/rated/movies`
     const sortMovies = { sort_by: 'created_at.desc' }
-    return this.getResource(url, page, null, sortMovies)
+
+    try {
+      const response = await this.getResource(url, page, null, sortMovies)
+      if (!response.results) {
+        throw new Error('Не удалось получить оцененные фильмы')
+      }
+      return response
+    } catch (error) {
+      console.error('Ошибка при получении оцененных фильмов:', error)
+      throw error
+    }
   }
 
-  getGenres = () => {
-    return this.getResource('/3/genre/movie/list')
+  getGenres = async () => {
+    try {
+      const response = await this.getResource('/3/genre/movie/list')
+      if (!response.genres) {
+        throw new Error('Не удалось получить жанры фильмов')
+      }
+      return response
+    } catch (error) {
+      console.error('Ошибка при получении жанров фильмов:', error)
+      throw error
+    }
   }
 }
